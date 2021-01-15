9255a4149d0041c7586e3644ae0aed4be2ae49ec
Jakub Cejchan <jakub.cejchan@socialbakers.com>
2021-01-15 11:35:42 +0100

    devtools added

--- redux/store.js
+++ redux/store.js
@@ -1,12 +1,14 @@
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import { basicLogger, delayActions, fooLogger } from './middleware/basicLogger';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import { basicLogger, delayActions } from './middleware/basicLogger';
 import licenceReducer from './reducers/licence-reducer';
 import userReducer from './reducers/user-reducer';
 
 const mainReducer = combineReducers({ licenceReducer, userReducer });
+//Redux devtools store enhancer [https://github.com/zalmoxisus/redux-devtools-extension]
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     mainReducer,
-    applyMiddleware(delayActions, basicLogger)
+    composeEnhancers(applyMiddleware(delayActions, basicLogger))
 );
 
 export default store;
